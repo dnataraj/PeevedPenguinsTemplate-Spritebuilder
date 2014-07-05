783477b8f43028cd9ebcaf9e8d2fabc9556d4e83
Deepak Natarajan <dnataraj@trilobytesystems.com>
2014-07-05 14:31:15 +0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -12,6 +12,9 @@
 
 - (void)play {
     CCLOG(@"Play button pressed...");
+    
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 
 @end
