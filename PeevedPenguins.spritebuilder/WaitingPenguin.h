c4f13a59d0008d1495b8252da874707d6f8cdc9f
Deepak Natarajan <dnataraj@trilobytesystems.com>
2014-08-05 11:34:30 +0300

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Deepak Natarajan on 5/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
